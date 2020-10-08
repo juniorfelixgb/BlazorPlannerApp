860ebe7890114b89407ba628f0af7f3ac5cca1c5
DESKTOP-TMIDRT3\Junior Felix <juniorfelixgb@gmail.com>
2020-10-07 20:33:58 -0400

    Implementando mail service con SendGrid, pendiente: corregir Authentication & Authorization en la parte de los CORS

---
+++ PlannerApp.API/Services/IMailService.cs
@@ -0,0 +1,30 @@
+﻿using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlannerApp.API.Services
+{
+    public interface IMailService
+    {
+        Task SendEmailAsync(string toEmail, string subject, string content);
+    }
+
+    public class SendGridMailService : IMailService
+    {
+        public async Task SendEmailAsync(string toEmail, string subject, string content)
+        {
+            var apiKey = Environment.GetEnvironmentVariable("");
+            var client = new SendGridClient(apiKey);
+            var from = new EmailAddress("", "");
+            subject = "";
+            var to = new EmailAddress("", "");
+            var plainTextContent = "";
+            var htmlContent = "<strong></strong>";
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+            _ = await client.SendEmailAsync(msg);
+        }
+    }
+}
