e898690e31e4ea3bcc324641706dcec171463676
DESKTOP-TMIDRT3\Junior Felix <juniorfelixgb@gmail.com>
2020-10-10 16:15:17 -0400

    add database

---
+++ PlannerApp.Database/dbo/Tables/Employees.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Employees] (
+    [Id]          INT            IDENTITY (1, 1) NOT NULL,
+    [FirstName]   NVARCHAR (MAX) NULL,
+    [LastName]    NVARCHAR (MAX) NULL,
+    [Email]       NVARCHAR (MAX) NULL,
+    [PhoneNumber] NVARCHAR (MAX) NULL,
+    CONSTRAINT [PK_Employees] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
